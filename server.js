4531c1fb5c285e48f830a7754979e2a8e6e16e3e
wesley guerra <guerra_ssa@hotmail.com>
2021-01-19 19:36:51 -0300

    configuração de envio de formulario

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 
 const app = express();
+const bodyParser = require("body-parser");
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false })); // permitir enviar dados do formulario decodificar
+app.use(bodyParser.json());
 
 // Express usar o EJS como View engine
+// rotas
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -13,6 +19,16 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
+});
+
+
+
+//
+app.post('/salvarpergunta', (req, res) => {
+    const { title, description } = req.body;
+
+    console.log(title)
+    console.log(description)
 });
 
 app.listen(3333, () => {
