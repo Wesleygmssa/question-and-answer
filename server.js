b3ee0b2baf674e0314783b9e924f9809afdf314a
wesley guerra <guerra_ssa@hotmail.com>
2021-01-20 09:01:55 -0300

    create a page response

--- server.js
+++ server.js
@@ -21,28 +21,53 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+
+//renderizando pagina
 app.get("/", async (req, res) => {
 
-    const perguntas = await PerguntaModel.findAll({ raw: true });
+    const perguntas = await PerguntaModel.findAll({ raw: true, order: [['id', 'DESC']] });
     console.log(perguntas)
     res.render("index", { perguntas });
 });
 
+//renderizando pagina
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
 });
 
-
-app.post('/salvarpergunta', (req, res) => {
+//salvando banco de dados
+app.post('/salvarpergunta', async (req, res) => {
     const { title, description } = req.body;
 
-    PerguntaModel.create({
+    await PerguntaModel.create({
         title,
         description
     }).then(() => {
         res.redirect('/');
     });
 
+
+});
+
+app.get('/pergunta/:id', async (req, res) => {
+    var id = req.params.id;
+    const pergunta = await PerguntaModel.findOne({ where: { id: id } })
+    if (pergunta != undefined) { //pergunta achada
+        res.render("pergunta", { pergunta });
+    } else { // não encontrada
+        res.redirect('/');
+    }
+
+    // PerguntaModel.findOne({
+    //     where: { id: id }
+    // }).then((pergunta) => {
+    //     console.log(pergunta)
+    //     if (pergunta != undefined) { //pergunta achada
+    //         res.render("pergunta");
+    //     } else { // não encontrada
+    //         res.redirect('/');
+    //     }
+    // })
 
 });
 
